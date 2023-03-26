e865cd4fd82c06b50b88f2d9b7fd4eee2850b233
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2023-03-26 15:11:38 -0400

    feat: init "resource" template with PlopJS

---
+++ plopfile.mjs
@@ -0,0 +1,48 @@
+import * as ChangeCase from 'change-case';
+
+export default function (
+  /** @type {import('plop').NodePlopAPI} */
+  plop,
+) {
+  plop.setHelper('camelCase', (txt) => ChangeCase.camelCase(txt));
+  plop.setHelper('pascalCase', (txt) => ChangeCase.pascalCase(txt));
+  plop.setHelper('snakeCase', (txt) => ChangeCase.snakeCase(txt));
+  plop.setHelper('paramCase', (txt) => ChangeCase.paramCase(txt));
+  plop.setHelper('constantCase', (txt) => ChangeCase.constantCase(txt));
+
+  plop.setGenerator('resource', {
+    description: 'application controller logic',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: "What's the resource name",
+      },
+    ],
+    actions: [
+      {
+        type: 'addMany',
+        destination: 'src/app/modules/{{ paramCase name }}',
+        base: `templates/resource/module`,
+        templateFiles: `templates/resource/module/**/*.hbs`,
+        skipIfExists: true,
+      },
+
+      {
+        type: 'addMany',
+        destination: 'src/app/repositories/',
+        base: `templates/resource/repositories`,
+        templateFiles: `templates/resource/repositories/**/*.hbs`,
+        skipIfExists: true,
+      },
+
+      {
+        type: 'addMany',
+        destination: 'src/app/entities/',
+        base: `templates/resource/entities`,
+        templateFiles: `templates/resource/entities/**/*.hbs`,
+        skipIfExists: true,
+      },
+    ],
+  });
+}
