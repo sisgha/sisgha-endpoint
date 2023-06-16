72d10935779f168e8268f61df1b34bfca88ddf07
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2023-06-16 12:56:06 -0400

    fix: plop: resource: import DatabaseModule

--- src/app/modules/permissao/permissao.module.ts
+++ src/app/modules/permissao/permissao.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database/database.module';
 import { MeiliSearchModule } from 'src/meilisearch/meilisearch.module';
 import { PermissaoResolver } from './permissao.resolver';
 import { PermissaoService } from './permissao.service';
 
 @Module({
   imports: [
+    DatabaseModule,
     MeiliSearchModule,
     // ...
   ],
--- src/app/modules/usuario_interno_cargo/usuario_interno_cargo.module.ts
+++ src/app/modules/usuario_interno_cargo/usuario_interno_cargo.module.ts
@@ -4,9 +4,11 @@ import { CargoModule } from '../cargo/cargo.module';
 import { UsuarioInternoModule } from '../usuario_interno/usuario_interno.module';
 import { UsuarioInternoCargoResolver } from './usuario_interno_cargo.resolver';
 import { UsuarioInternoCargoService } from './usuario_interno_cargo.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   imports: [
+    DatabaseModule,
     MeiliSearchModule,
     // ...
     CargoModule,
