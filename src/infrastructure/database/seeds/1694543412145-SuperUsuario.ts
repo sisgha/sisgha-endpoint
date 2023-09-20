ef8a21a3c3e29ce582bfd7d7e94ada1ff08b6919
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2023-09-20 13:55:33 -0400

    fix: seed SuperUsuario: find cargo sistema using repository directly

--- src/infrastructure/database/seeds/1694543412145-SuperUsuario.ts
+++ src/infrastructure/database/seeds/1694543412145-SuperUsuario.ts
@@ -3,13 +3,13 @@ import { DataSource, MigrationInterface, QueryRunner } from 'typeorm';
 import { IAppResource } from '../../../domain/application-resources';
 import { ActorContext } from '../../actor-context/ActorContext';
 import { AppModule } from '../../application/app.module';
-import { CargoService } from '../../application/resources/cargo/cargo.service';
 import { UsuarioResource } from '../../application/resources/usuario/usuario.resource';
 import { EnvironmentConfigService } from '../../config/environment-config/environment-config.service';
 import { KCClientService } from '../../kc-container/kc-client.service';
 import { MeiliSearchService } from '../../meilisearch/meilisearch.service';
 import { UsuarioDbEntity } from '../entities/usuario.db.entity';
 import { UsuarioCargoDbEntity } from '../entities/usuario_cargo.db.entity';
+import { getCargoRepository } from '../repositories/cargo.repository';
 import { getUsuarioRepository } from '../repositories/usuario.repository';
 import { getUsuarioCargoRepository } from '../repositories/usuario_cargo.repository';
 import { APP_DATA_SOURCE_TOKEN } from '../tokens/APP_DATA_SOURCE_TOKEN';
@@ -48,9 +48,14 @@ export class SuperUsuario1694543412145 implements MigrationInterface {
 
     //
 
-    const cargoService = app.get(CargoService);
+    const cargoRepository = getCargoRepository(queryRunner.manager);
 
-    const cargoSistema = await cargoService.findCargoBySlugStrictSimple(actorContext, 'sistema');
+    const cargoSistema = await cargoRepository.findOneOrFail({
+      where: {
+        slug: 'sistema',
+      },
+      select: ['id'],
+    });
 
     const usuarioCargoRepository = getUsuarioCargoRepository(queryRunner.manager);
 
